455681eb86f06e939cd688a6127d2c234410582c
cristinarodriguezl-dev <cristinarodriguez.cv@gmail.com>
2026-09-18 10:33:04 +0200

    feat: add catalog module and implement interactive mineral input loop in main

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+from catalog import catalog 
+
 print("Bienvenido/a al Catálogo de colección de Minerales y Piedras.")
 
+#Piezas
+for i in range (10):
+    id = input("Ingrese el ID de la pieza: ")
+    name = input("Ingrese el nombre de la pieza: ")
+    category = input("¿Qué tipo de piedra o mineral es?: ")
+    price = input("Ingrese el precio estimado: ")
+    status = input("Ingrese el estado de la pieza: ")
+    description = input("Describa brevemente la pieza: ")
+    
+    piezas = {
+    "id" : id,
+    "name" : name,
+    "category" : category,
+    "price" : price,
+    "status" : status,
+    "description" : description
+    }
+    
+    catalog.append(piezas)
+
+print(catalog)
